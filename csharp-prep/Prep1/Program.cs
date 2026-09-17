9f472a08cf837af9233e4a7f76a385cdc57bdc62
KyleBlackson <bla24045@byui.edu>
2026-09-17 11:15:50 -0600

    Added Name

--- csharp-prep/Prep1/Program.cs
+++ csharp-prep/Prep1/Program.cs
@@ -4,6 +4,13 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep1 World!");
+        string firstName;
+        Console.Write("Please enter your name: ");
+        firstName = Console.ReadLine();
+        string lastName;
+        Console.Write("Please enter your last name: ");
+        lastName = Console.ReadLine();
+
+        Console.WriteLine($"{firstName}{lastName}");
     }
 }
