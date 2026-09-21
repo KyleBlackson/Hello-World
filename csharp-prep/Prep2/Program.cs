08095a6b5b17b65284fb335b52782acac78f5905
KyleBlackson <bla24045@byui.edu>
2026-09-21 14:09:55 -0600

    Added Program for Grade Analyzer

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -4,6 +4,39 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        int gradeScore;
+        string userInput;
+        Console.WriteLine("Please Enter Your Grade Score: ");
+        userInput = Console.ReadLine ();
+        gradeScore = int.Parse(userInput);
+
+        if (gradeScore >= 90)
+        {
+            Console.WriteLine("You got an A");
+        }
+        else if (gradeScore >= 80)
+        {
+            Console.WriteLine("You got a B");
+        }
+        else if (gradeScore >= 70)
+        {
+            Console.WriteLine("You got a C");
+        }
+        else if (gradeScore >= 60)
+        {
+            Console.WriteLine("You got a D");
+        }
+        else
+        {
+            Console.WriteLine("You got an F");
+        }
+        if (gradeScore >= 70)
+        {
+            Console.WriteLine("Congratulations! You passed the class");
+        }
+        else
+        {
+            Console.WriteLine("You didn't pass the class, but better luck next time!");
+        }
     }
 }
