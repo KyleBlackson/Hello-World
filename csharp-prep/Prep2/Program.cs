cbfb1844dda581dcd9deee37b7f84ede03814c06
KyleBlackson <bla24045@byui.edu>
2026-09-21 16:53:11 -0600

    Included Code for + or -

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -1,42 +1,73 @@
 using System;
+using System.Reflection.Metadata;
 
 class Program
 {
     static void Main(string[] args)
     {
-        int gradeScore;
+        double gradeScore;
         string userInput;
         Console.WriteLine("Please Enter Your Grade Score: ");
         userInput = Console.ReadLine ();
-        gradeScore = int.Parse(userInput);
-
-        if (gradeScore >= 90)
-        {
-            Console.WriteLine("You got an A");
-        }
-        else if (gradeScore >= 80)
-        {
-            Console.WriteLine("You got a B");
-        }
-        else if (gradeScore >= 70)
-        {
-            Console.WriteLine("You got a C");
-        }
-        else if (gradeScore >= 60)
-        {
-            Console.WriteLine("You got a D");
-        }
-        else
-        {
-            Console.WriteLine("You got an F");
-        }
-        if (gradeScore >= 70)
-        {
+        gradeScore = double.Parse(userInput);
+        string letterGrade;
+        string plusOrMinus;
+            if (gradeScore >= 90)
+            {
+            letterGrade = "A";
+            }
+            else if (gradeScore >= 80)
+            {
+            letterGrade = "B";
+            }
+            else if (gradeScore >= 70)
+            {
+            letterGrade = "C";
+            }
+            else if (gradeScore >= 60)
+            {
+            letterGrade = "D";
+            }
+            else
+            {
+            letterGrade = "F";
+            }
+            if (gradeScore >= 70)
+            {
             Console.WriteLine("Congratulations! You passed the class");
+            }
+            else
+            {
+            Console.WriteLine("You didn't pass the class, but better luck next time!");
+            }
+        if (gradeScore >= 97)
+        {
+            Console.WriteLine(letterGrade);
+        }
+
+        else if (gradeScore <= 63)
+        {
+            Console.WriteLine(letterGrade);
+
         }
         else
         {
-            Console.WriteLine("You didn't pass the class, but better luck next time!");
+        {
+        gradeScore = gradeScore%10;
+        if (gradeScore >=7)
+        {
+            plusOrMinus = "+";
+        }
+        else if (gradeScore <= 3)
+        {
+            plusOrMinus = "-";
+        }
+        else
+        {
+            plusOrMinus = "";
+        }
+        Console.WriteLine(letterGrade+plusOrMinus);
+        }
         }
     }
 }
