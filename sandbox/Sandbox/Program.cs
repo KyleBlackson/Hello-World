fbbb4a4d3723a435fb886c99bfe8a347a5950e4d
KyleBlackson <bla24045@byui.edu>
2026-09-22 09:43:48 -0600

    Changed to be a simple else else if program

--- sandbox/Sandbox/Program.cs
+++ sandbox/Sandbox/Program.cs
@@ -1,11 +1,24 @@
 using System;
+using System.Security.Cryptography;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Bonjour tout le monde");
-        Console.WriteLine("Hey Jude.")
-        Console.WriteLine("Hey Jay")
+        int x = 20;
+
+        if (x == 10)
+        {
+            Console.WriteLine("X is 10");
+            Console.WriteLine("Y is fun");
+        }
+        else if (x == 20)
+        {
+            Console.WriteLine("X is 20");
+        }
+        else
+        {
+            Console.WriteLine("Default output");
+        }
     }
 }
