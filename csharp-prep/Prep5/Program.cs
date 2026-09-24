6d74f8fee9fd54dd9e28190c482bf8fbf7946b98
KyleBlackson <bla24045@byui.edu>
2026-09-24 13:48:47 -0600

    Added Functions

--- csharp-prep/Prep5/Program.cs
+++ csharp-prep/Prep5/Program.cs
@@ -4,16 +4,41 @@ class Program
 {
     static void Main(string[] args)
     {
+        static void Welcome ()
+        {
         Console.WriteLine("Welcome to the Program");
+        }
+        static string EnterName (string name1)
+        {
         Console.Write("Please enter your name: ");
         string name = Console.ReadLine();
+        return name;
+        }
+        static int numSquared(int square1)
+        {
         Console.Write("Please enter you favorite number: ");
         string blank = Console.ReadLine();
         int favnum = int.Parse(blank);
+        int square = favnum*favnum;
+        return square;
+        }
+        static int futureAge(int agePlusOne)
+        {
         Console.Write("Please enter your birth year: ");
         string blankOne = Console.ReadLine();
         int birthYear = int.Parse(blankOne);
-        Console.WriteLine(name+", the square of your number is "+favnum*favnum);
-        Console.WriteLine(name+", you will turn "+(2026-birthYear)+" this year");
-    }
+        int newAge = 2026-birthYear;
+        return newAge;
+        }
+        Welcome();
+        static void DisplayMessage()
+        {
+        string name = EnterName("f");
+        int newAge = futureAge(1);
+        int square = numSquared(1);
+        Console.WriteLine(name+", the square of your number is "+square);
+        Console.WriteLine(name+", you will turn "+newAge+" this year");
+        }
+        DisplayMessage();
+        }
 }
