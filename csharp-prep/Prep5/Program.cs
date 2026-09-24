1abb0e3e0c4a68c7557a3945fea0074e6a808ebb
KyleBlackson <bla24045@byui.edu>
2026-09-24 10:07:56 -0600

    Built the basic Program

--- csharp-prep/Prep5/Program.cs
+++ csharp-prep/Prep5/Program.cs
@@ -4,6 +4,16 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep5 World!");
+        Console.WriteLine("Welcome to the Program");
+        Console.Write("Please enter your name: ");
+        string name = Console.ReadLine();
+        Console.Write("Please enter you favorite number: ");
+        string blank = Console.ReadLine();
+        int favnum = int.Parse(blank);
+        Console.Write("Please enter your birth year");
+        string blankOne = Console.ReadLine();
+        int birthYear = int.Parse(blankOne);
+        Console.WriteLine(name+", the square of your number is"+favnum*favnum);
+        Console.WriteLine(name+", you will turn"+(2026-birthYear)+" this year");
     }
 }
--- sandbox/Sandbox/Program.cs
+++ sandbox/Sandbox/Program.cs
@@ -1,27 +1,49 @@
 using System;
 using System.IO.Compression;
+using System.Reflection.Metadata;
 using System.Security.Cryptography;
 
 class Program
 {
     static void Main(string[] args)
     {
-        int x = 20;
-        int y = 30;
-        int z = 40;
+    //     int x = 20;
+    //     int y = 30;
+    //     int z = 40;
 
-        if (x == 10 && y ==30 || z ==40)
+    //     if (x == 10 && y ==30 || z ==40)
+    //     {
+    //         Console.WriteLine("X is 10");
+    //         Console.WriteLine("Y is fun");
+    //     }
+    //     else if (x == 20)
+    //     {
+    //         Console.WriteLine("X is 20");
+    //     }
+    //     else
+    //     {
+    //         Console.WriteLine("Default output");
+    //     }
+
+    // bool done = false;
+
+    // do
+    //     {
+    //         Console.Write("Are we done (y/n)? ");
+    //         done = Console.ReadLine().ToLower() == "y";
+    //     }while (! done);
+
+    for(int i = 100000; i >= -100000; i-=100000)
         {
-            Console.WriteLine("X is 10");
-            Console.WriteLine("Y is fun");
+            Console.WriteLine(i);
         }
-        else if (x == 20)
+
+    List <string> myFriends = new List<string> {"Bob", {"Betty"},"Bubba"};
+
+    myFriends.Add("Doug");
+    foreach(string friend in myFriends)
         {
-            Console.WriteLine("X is 20");
-        }
-        else
-        {
-            Console.WriteLine("Default output");
+            Console.WriteLine(friend);
         }
     }
 }
